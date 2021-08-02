9ca6128b8620de94804f5c828695795de1b5276c
zacktcheng <zaizizheng@gmail.com>
2021-08-02 14:18:37 -0700

    add position api into get function

--- index.js
+++ index.js
@@ -9,7 +9,16 @@ const app = express();
 app.use(cors());
 
 app.get('/n2yo', async (req, res) => {
-  const url = `https://api.n2yo.com/rest/v1/satellite/${req.query.api}/${req.query.latitude}/${req.query.longitude}/${req.query.altitude}/${req.query.radius}/${req.query.category}/&apiKey=${req.query.apikey}`;
+  
+  let url = '';
+  
+  if (req.query.api === 'above') {
+    url = `https://api.n2yo.com/rest/v1/satellite/above/${req.query.latitude}/${req.query.longitude}/${req.query.altitude}/${req.query.radius}/${req.query.category}/&apiKey=${req.query.apikey}`;
+  }
+  if (req.query.api === 'positions') {
+    url = `https://api.n2yo.com/rest/v1/satellite/positions/${req.query.id}/${req.query.latitude}/${req.query.longitude}/${req.query.altitude}/${req.query.duration * 60}/&apiKey=${req.query.apikey}`
+  }
+  
   const options = { 'method': 'GET' };
   const response = await fetch(url, options)
    .then(res => res.json())
@@ -19,8 +28,8 @@ app.get('/n2yo', async (req, res) => {
       error: e
     }); 
    }); 
-
-  res.send(response);
+  
+   res.send(response);
 });
 
 app.listen(port, () => {
